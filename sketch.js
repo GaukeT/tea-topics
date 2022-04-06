516b134f7b43b1f73a5380dcf0a7f45f770c4467
Gauke Teijema <gauke.teijema@iddinkgroup.com>
2022-04-06 15:45:58 +0200

    add icon / show button when language is selected / text changes

--- sketch.js
+++ sketch.js
@@ -2,15 +2,20 @@ let lang;
 let stop;
 let isStopped = false;
 let choice = 0;
-let quest = ['Welcome at TeaTopics! Choose a language to start with...'];
-let emptyState = 'Choose an option';
+let quest = ['Welcome at TeaTopics! Choose a language to start.'];
+let emptyState = 'Choose a language';
+let tea;
+
+function preload() {
+  tea = loadImage('cup-of-tea.png');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(10);
   
   lang = createSelect();
-  lang.position(width - 140, 10);
+  lang.position(width - 150, 10);
   lang.option(emptyState);
   lang.disable(emptyState);
   lang.selected(emptyState);
@@ -18,28 +23,27 @@ function setup() {
   lang.option('Dutch');
   lang.option('English');
   lang.changed(selectLang);
-  
-  stop = createButton('Stop / Start');
-  stop.center();
-  stop.mousePressed(stopMe)
 }
 
 function draw() {
   background(255);
-  textFont('Verdana', 12);
+  textFont('Verdana', 16);
   textStyle(NORMAL);
   
   if (!isStopped) {
     choice = int(random(quest.length))
   } else {
     if (lang.value() != emptyState) {
-      textSize(18);
+      textSize(20);
       textStyle(BOLD);
     }
   }
     
   textAlign(CENTER);
   text(quest[choice], width/2, height/2 - 50);
+
+  tint(255, 55);
+  image(tea, width/2 - 115, height/2 - 300, 250, 250);
 }
 
 function stopMe() {
@@ -58,11 +62,19 @@ function selectLang() {
     quest = quest;
   }
   
+  if (!stop) {
+    stop = createButton('Stop - Start');
+    stop.center();
+    stop.mousePressed(stopMe)
+  }
+
   isStopped = false;
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  lang.position(width - 140, 10);
-  stop.center();
+  lang.position(width - 150, 10);
+  if (stop) {
+    stop.center();
+  }
 }
